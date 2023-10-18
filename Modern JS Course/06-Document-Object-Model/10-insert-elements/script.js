53399a4d8d228a8c30f271bafa8ede79d0b8653e
Tasnim Alam <tasnim.alam.015@gmail.com>
2023-10-18 17:40:24 +0600

    commit

---
+++ Modern JS Course/06-Document-Object-Model/10-insert-elements/script.js
@@ -0,0 +1,55 @@
+/*
+<!-- beforebegin -->
+<p>
+  <!-- afterbegin -->
+  foo
+  <!-- beforeend -->
+</p>
+<!-- afterend -->
+*/
+
+
+// insertAdjacentElement Example
+function insertElement(){
+  const filter = document.querySelector('.filter')
+
+  const h1 = document.createElement('h1')
+  h1.textContent = 'insertAdjacentElement'
+
+  filter.insertAdjacentElement('beforebegin', h1)
+  // filter.insertAdjacentElement('afterend', h1)
+}
+
+insertElement()
+
+
+// insertAdjacentText Example
+function insertText(){
+  const item =document.querySelector('li')
+
+  item.insertAdjacentText('beforebegin', 'insertAdjacentText')
+
+}
+insertText()
+
+
+// insertAdjacentHTML example
+function insertHTML(){
+  const clearBtn = document.querySelector('#clear')
+
+  clearBtn.insertAdjacentHTML('beforebegin', '<h2>insertAdjacentHTML</h2>')
+} 
+insertHTML()
+
+// insertBefore Example
+function insertBeforeItem(){
+  const ul = document.querySelector('ul')
+  
+  const li = document.createElement('li')
+  li.textContent = 'insertBefore'
+
+  const thirdItem = document.querySelector('li:nth-child(3)')
+
+  ul.insertBefore(li, thirdItem)
+}
+insertBeforeItem()
