7364fb850e0b85b0743b8c55455065a20b6dc13c
Tasnim Alam <tasnim.alam.015@gmail.com>
2023-10-26 11:21:28 +0600

    commit

--- Modern JS Course/08-Asynchronous-Javascript/10-Sequential-Data-Processing/script.js
+++ Modern JS Course/08-Asynchronous-Javascript/10-Sequential-Data-Processing/script.js
@@ -1,3 +1,9 @@
+// In Case 1, you have a single userInfo function that fetches user data, todos, and posts one after the other sequentially. The key points are:
+// Each data fetch operation (fetchData) is performed one at a time, waiting for the previous one to complete before starting the next.
+// If any of the fetch operations fail (e.g., due to network issues or response errors), an error is caught and logged, but the subsequent fetch operations continue.
+// This approach is straightforward and ensures that data is fetched in a specific order. However, it can be slow when dealing with multiple API calls, as it doesn't take full advantage of parallelism.
+
+/*
 async function fetchData(url){
     try{
         const response = await fetch(url)
@@ -23,8 +29,55 @@ async function userInfo(){
 }
 
 userInfo()
+*/
 
-// In Case 1, you have a single userInfo function that fetches user data, todos, and posts one after the other sequentially. The key points are:
-// Each data fetch operation (fetchData) is performed one at a time, waiting for the previous one to complete before starting the next.
-// If any of the fetch operations fail (e.g., due to network issues or response errors), an error is caught and logged, but the subsequent fetch operations continue.
-// This approach is straightforward and ensures that data is fetched in a specific order. However, it can be slow when dealing with multiple API calls, as it doesn't take full advantage of parallelism.
+
+async function fetchData(url) {
+    try {
+        const response = await fetch(url)
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status:${response.status}`);
+        }
+
+        const userData = await response.json();
+        return userData;
+    }
+    catch (error) {
+        console.error('An error occurred while fetching user data', error);
+        return []
+    }
+}
+
+function populateHTML(populate, cl) {
+    const main = document.querySelector('main')
+    const div = document.createElement('div');
+    const p = document.createElement('p');
+    const textNode = document.createTextNode(populate);
+    p.classList = cl
+
+    p.append(textNode); 
+    div.append(p); 
+    main.append(div)
+}
+
+async function populateUserName() {
+    const users = await fetchData('https://jsonplaceholder.typicode.com/users')
+    console.log(users)
+
+    for(const user of users){
+        populateHTML(user.name, 'name')
+    }
+}
+
+async function populateUserPost() {
+    const posts = await fetchData('https://jsonplaceholder.typicode.com/posts')
+    
+    
+    for(const post of posts){
+        populateHTML(post.body, 'post')
+    }
+}
+
+populateUserName()
+populateUserPost()
