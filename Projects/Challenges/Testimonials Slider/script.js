6fe1b9979f310ecd7fb004c1cf3c98eaed59e57e
Tasnim Alam <tasnim.alam.015@gmail.com>
2023-11-09 22:04:11 +0600

    Testimonials Slider Challenge Completed

--- Projects/Challenges/Testimonials Slider/script.js
+++ Projects/Challenges/Testimonials Slider/script.js
@@ -1,29 +1,46 @@
-const prevButton = document.querySelector('#previous')
-const nextButton = document.querySelector('#next')
+const prevButton = document.querySelectorAll('#previous');
+const nextButton = document.querySelectorAll('#next');
 
-const wrapper = document.querySelectorAll('.wrapper')
+const wrapper = document.querySelectorAll('.wrapper');
+let i = 0;
 
-// wrapper[1].style.display = 'block'
+// Initially, apply the 'active' class to the first testimonial
+wrapper[i].classList.add('active');
 
-nextButton.addEventListener('click', forward)
-prevButton.addEventListener('click', backward)
+nextButton.forEach((item)=>{
+    item.addEventListener('click', forward);
+})
 
-let i = 0
+prevButton.forEach((item)=>{
+    item.addEventListener('click', backward);
+})
+
+
 
 function forward() {
-    i++
-    // wrapper[i].classList.toggle('active')
-    // console.log(i)
-    updateIndex()
+    // Remove 'active' class from the current testimonial
+    wrapper[i].classList.remove('active');
+    i++;
+
+    // Check if index is out of bounds and reset it to 0 if necessary
+    if (i >= wrapper.length) {
+        i = 0;
+    }
+
+    // Add 'active' class to the next testimonial
+    wrapper[i].classList.add('active');
 }
 
 function backward() {
-    i--
-    // console.log(i)
-    updateIndex()
-}
+    // Remove 'active' class from the current testimonial
+    wrapper[i].classList.remove('active');
+    i--;
 
-function updateIndex() {
-    const index = i
-    wrapper[index].classList.toggle('active')
+    // Check if index is out of bounds and set it to the last testimonial if necessary
+    if (i < 0) {
+        i = wrapper.length - 1;
+    }
+
+    // Add 'active' class to the previous testimonial
+    wrapper[i].classList.add('active');
 }
