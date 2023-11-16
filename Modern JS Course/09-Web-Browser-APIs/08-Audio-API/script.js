b05d977dd9543ebd40fd2f72bc7c9455712f3e11
Tasnim Alam <tasnim.alam.015@gmail.com>
2023-11-16 21:19:16 +0600

    commit

--- Modern JS Course/09-Web-Browser-APIs/08-Audio-API/script.js
+++ Modern JS Course/09-Web-Browser-APIs/08-Audio-API/script.js
@@ -1,35 +1,72 @@
 const audio = document.getElementById('audio');
 const play = document.getElementById('play');
-const pause = document.getElementById('pause');
-const stop = document.getElementById('stop');
-const currentTime = document.getElementById('current-time');
+const forward = document.querySelector('#forward')
+const replay = document.querySelector('#replay')
+
+const seekBar = document.getElementById('seekBar');
+const timePassed = document.querySelector('#timePassed')
+const totalDuration = document.querySelector('#totalDuration')
+
 const volume = document.getElementById('volume');
 
 
+play.addEventListener('click', () => {
+  if (audio.paused) {
+    audio.play();
+    play.querySelector('img').setAttribute('src', 'images/pause.svg');
+  } else {
+    audio.pause();
+    play.querySelector('img').setAttribute('src', 'images/play_circle.svg');
+  }
+});
 
 
+// Volume
+volume.addEventListener('input', () => {
+  const newVolume = volume.value;
+  audio.volume = newVolume;
 
-
-
-
-play.addEventListener('click', () => audio.play());
-pause.addEventListener('click', () => audio.pause());
-stop.addEventListener('click', () => {
-  audio.pause();
-  audio.currentTime = 0;
+  if (audio.volume === 0) {
+    document.querySelector('.vol img').setAttribute('src', 'images/mute.svg');
+  } else {
+    document.querySelector('.vol img').setAttribute('src', 'images/volume.svg');
+  }
 });
 
 
 
-volume.addEventListener('change', () => (audio.volume = volume.value));
+// Playback
+audio.addEventListener('timeupdate', () => {
+  // Update the current time input during audio playback
+  seekBar.value = audio.currentTime;
+  timePassed.textContent = formatTime(audio.currentTime)
+});
 
-  // Event listener for the input event on the currentTime range input
-  currentTime.addEventListener('input', () => {
-    // Set the currentTime of the audio to the value of the range input
-    audio.currentTime = currentTime.value;
-  });
+// Update on drag and start playing
+seekBar.addEventListener('input', () => {
+  audio.currentTime= seekBar.value
+});
 
-  // Update the currentTime range when the time of the audio changes
-  audio.addEventListener('timeupdate', () => {
-    currentTime.value = audio.currentTime;
-  });
+// Get total duration of an audio file and displaying it
+audio.addEventListener('loadedmetadata', () => {
+  totalDuration.textContent = formatTime(audio.duration);
+
+  // Set Max Attribute of input range for seekBar
+  seekBar.setAttribute('max', Math.floor(audio.duration))
+});
+
+function formatTime(time) {
+  // Format time in HH:MM:SS
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
+
+// Forward and Replay
+forward.addEventListener('click',()=>{
+  audio.currentTime= audio.currentTime+=10
+})
+
+replay.addEventListener('click', ()=>{
+  audio.currentTime= audio.currentTime-=10
+})
