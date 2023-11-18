bc0f5ac7b0867eb6ee21014cd0e4ec34005a96a5
Tasnim Alam <tasnim.alam.015@gmail.com>
2023-11-19 00:23:51 +0600

    Commit

---
+++ Modern JS Course/10-OOP Old/01-Factory & Constructor Function/script.js
@@ -0,0 +1,23 @@
+// Factory Function
+function createCircle(radius){
+    return{
+        radius,
+        draw: function(){
+            console.log('draw')
+        }
+    }
+}
+
+const circle = createCircle(1)
+// circle.draw()
+
+// Constructor Function
+function Circle(radius){
+    // console.log('this', this)
+    this.radius = radius;
+    this.draw = function(){
+        console.log('Draw')
+    }
+}
+
+const another = new Circle(1)
