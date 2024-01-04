2b32653ce4e0bac0f046560bd2c241a28425cf7e
Tasnim Alam <tasnim.alam.015@gmail.com>
2024-01-04 22:22:14 +0600

    nodemon deleted

--- Express Crash Course/01-Web-Dev-Simplified/01-Intro/server.js
+++ Express Crash Course/01-Web-Dev-Simplified/01-Intro/server.js
@@ -2,6 +2,11 @@ import express from 'express';
 const app = express();
 
 app.listen(3000)
+app.get('/', (req, res) => {
 
-console.log('H')
-console.log('Hello')
+})
+
+console.log('dd')
+console.log('df')
+
+console.log('hello')
