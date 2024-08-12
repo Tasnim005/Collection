c3b6fba86079ba889e08bb9df5d98e0ff159f3e5
Tasnim Alam <tasnim.alam.015@gmail.com>
2024-08-13 00:08:57 +0600

    Verse App created

---
+++ Projects/Verses App/script.js
@@ -0,0 +1,48 @@
+const slides = document.querySelectorAll('.slide')
+
+const prevButton = document.querySelectorAll('#previous');
+const nextButton = document.querySelectorAll('#next');
+
+
+let i = 0;
+
+// Initially, apply the 'active' class to the first testimonial
+slides[i].classList.add('active');
+
+nextButton.forEach((item)=>{
+    item.addEventListener('click', forward);
+})
+
+prevButton.forEach((item)=>{
+    item.addEventListener('click', backward);
+})
+
+
+
+function forward() {
+    // Remove 'active' class from the current testimonial
+    slides[i].classList.remove('active');
+    i++;
+
+    // Check if index is out of bounds and reset it to 0 if necessary
+    if (i >= slides.length) {
+        i = 0;
+    }
+
+    // Add 'active' class to the next testimonial
+    slides[i].classList.add('active');
+}
+
+function backward() {
+    // Remove 'active' class from the current testimonial
+    slides[i].classList.remove('active');
+    i--;
+
+    // Check if index is out of bounds and set it to the last testimonial if necessary
+    if (i < 0) {
+        i = slides.length - 1;
+    }
+
+    // Add 'active' class to the previous testimonial
+    slides[i].classList.add('active');
+}
